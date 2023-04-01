20ed81742e95d9382e6b2280528d42da8665a622
Crowdbotics <engineering@crowdbotics.com>
2023-04-01 00:14:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-glade-dev-73526",
+  url: "https://tight-glade-dev-73526.botics.co",
+  api: "https://tight-glade-dev-73526.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
